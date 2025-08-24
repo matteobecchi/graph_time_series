85c40f80c9538364d5398b29a4dccf2729f3e327
matteobecchi <bechmath@gmail.com>
2025-08-24 17:44:03 +0200

    Some small changes in the docstring.

--- src/graph_time_series/_internal/observables.py
+++ src/graph_time_series/_internal/observables.py
@@ -11,23 +11,36 @@ if TYPE_CHECKING:
 import networkx as nx
 
 
-def degree(graph: Graph) -> dict[str, float]:
-    """Return a dict of node degrees (weighted if graph is weighted)."""
+def degree(graph: Graph) -> dict[int, float]:
+    """Return a dict of node degrees (weighted if graph is weighted).
+
+    Parameters
+    ----------
+    graph: graph_time_series.Graph
+        The graph we want to compute the nodes' degrees.
+    """
     return dict(graph.nx_graph.degree(weight="weight"))
 
 
-def clustering(graph: Graph) -> dict[str, float]:
+def clustering(graph: Graph) -> dict[int, float]:
     """Return clustering coefficients per node.
 
-    Parameters:
-        graph: graph_time_series.Graph
-            The graph we want to compute the nodes' degrees.
+    Parameters
+    ----------
+    graph: graph_time_series.Graph
+        The graph we want to compute the nodes' degrees.
     """
     return dict(nx.clustering(graph.nx_graph.to_undirected(), weight="weight"))
 
 
 def diameter(graph: Graph) -> int:
-    """Return the diameter of the graph."""
+    """Return the diameter of the graph.
+
+    Parameters
+    ----------
+    graph: graph_time_series.Graph
+        The graph we want to compute the nodes' degrees.
+    """
     if nx.is_connected(graph.nx_graph.to_undirected()):
         return nx.diameter(graph.nx_graph.to_undirected())
     msg = "Graph is not connected."
